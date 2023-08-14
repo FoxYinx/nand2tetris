b1293bfb0bbad80ef19f29a31d708de1fc3fb408
joshuadwightred <tollemerkevin@gmail.com>
2023-08-14 17:27:25 +0200

    The assembler works for simple program

--- projects/06/HackAssembler.java
+++ projects/06/HackAssembler.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class HackAssembler {
 
@@ -7,10 +8,12 @@ public class HackAssembler {
         try {
             File file = new File("add/add.asm");
             Parser myParser = new Parser(file);
-            myParser.readLines(myParser.getScanner());
+            myParser.readLinesAndWrite(myParser.getScanner(), "add");
             myParser.closeFile(myParser.getScanner());
         } catch (FileNotFoundException e){
             System.out.println("The file wasn't found");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
--- projects/06/Parser.java
+++ projects/06/Parser.java
@@ -1,20 +1,42 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Parser {
 
     private Scanner scanner;
+    private Code code;
 
-    Parser (File file) throws FileNotFoundException {
+    public Parser (File file) throws FileNotFoundException {
         this.scanner = new Scanner(file);
+        this.code = new Code();
     }
 
-    public void readLines(Scanner scanner){
+    public void readLinesAndWrite(Scanner scanner, String fileName) throws IOException {
+        String output;
+        File file = new File("add/" + fileName + ".hack");
+        file.delete();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("add/" + fileName + ".hack", true));
         while(scanner.hasNextLine()){
             String data = scanner.nextLine();
-            System.out.println(data);
+            data = data.split("//")[0];
+            if(data.isEmpty()) continue;
+            if (data.charAt(0)=='@'){
+                output = code.toBinary(data.split("@")[1]);
+            } else {
+                String dest = code.toDest(data.split("=")[0]);
+                String comp = code.toComp(data.split("=")[1].split(";")[0]);
+                String jump;
+                if (dest.split(";").length !=1){
+                    jump = code.toJump(data.split("=")[1].split(";")[1]);
+                } else {
+                    jump = code.toJump(null);
+                }
+                output = "111" + comp + dest + jump;
+            }
+            writer.append(output);
+            writer.newLine();
         }
+        writer.close();
     }
 
     public void closeFile(Scanner scanner){
